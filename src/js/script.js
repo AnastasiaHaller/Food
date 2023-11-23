ce6d5c9180f2ec82dafd461699c013bb2fcd997d
Anastasia Haller <anadana87@gmail.com>
2023-11-23 07:53:07 +0100

    heroku

--- src/js/script.js
+++ src/js/script.js
@@ -153,13 +153,14 @@ window.addEventListener('DOMContentLoaded', () => {
     // Cards
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
             this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
             this.exchange = 0.9;
             this.changeToEUR();
         }
@@ -170,16 +171,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Price:</div>
-                        <div class="menu__item-total"><span>${this.price}</span>EUR/day</div>
-                    </div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Price:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> EUR/day</div>
                 </div>
             `;
 
@@ -194,7 +201,8 @@ window.addEventListener('DOMContentLoaded', () => {
         "Fitness",
         '"Fitness" menu is a new approach to cooking: more fresh vegetables and fruits. For people who are interested in sports; active and healthy. This is a completely new product with the best price and high quality!',
         15,
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
 
     new MenuCard(
@@ -203,7 +211,8 @@ window.addEventListener('DOMContentLoaded', () => {
         "Lenten",
         'Our special “Lenten menu” is a careful selection of ingredients: a complete absence of animal products. Complete harmony with yourself and nature in every element! Everything will be Om!',
         20,
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
 
     new MenuCard(
@@ -212,7 +221,8 @@ window.addEventListener('DOMContentLoaded', () => {
         "Premium",
         '“Premium” menu - we use not only beautiful packaging design, but also high-quality execution of dishes. Red fish, seafood, fruits - a restaurant menu without going to a restaurant!',
         24,
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
 
 });
